dd8dfcfb4b19fa163066d368b6683e18b80f131f
Jacob Lewallen <jlewallen@gmail.com>
2018-06-26 12:24:13 -0700

    Make large FileIndex spec use less memory.

--- test/testall/test_preallocated.cpp
+++ test/testall/test_preallocated.cpp
@@ -318,13 +318,9 @@ TEST_F(PreallocatedSuite, RollingWriteStrategyIndexWraparound) {
 
 class FileIndexSuite : public ::testing::Test {
 protected:
-    Geometry geometry_{ 1024 * 1024, 4, 4, 512 };
-    LinuxMemoryBackend storage_;
 
 protected:
     void SetUp() override {
-        ASSERT_TRUE(storage_.initialize(geometry_));
-        ASSERT_TRUE(storage_.open());
     }
 
     void TearDown() override {
@@ -333,20 +329,29 @@ protected:
 };
 
 TEST_F(FileIndexSuite, LargeIndex) {
-    FileAllocation allocation{ { 1, 10000 }, { 0, 1 } };
-    FileIndex index{ &storage_, &allocation };
-    BlockHelper helper{ storage_ };
+    auto number_of_index_entries = 1024 * 1024;
+    auto bytes_required = number_of_index_entries * sizeof(IndexRecord);
+    auto blocks_required = uint32_t(bytes_required / ((4 * 4 * SectorSize) - SectorSize - SectorSize)) * 2;
+
+    Geometry geometry{ blocks_required, 4, 4, 512 };
+    LinuxMemoryBackend storage;
+    FileAllocation allocation{ { 1, blocks_required }, { 0, 1 } };
+    FileIndex index{ &storage, &allocation };
+    BlockHelper helper{ storage };
+
+    ASSERT_TRUE(storage.initialize(geometry));
+    ASSERT_TRUE(storage.open());
 
     ASSERT_TRUE(index.format());
 
-    ASSERT_EQ(helper.number_of_blocks(BlockType::Index, 0, 10000), 1);
+    ASSERT_EQ(helper.number_of_blocks(BlockType::Index, 0, blocks_required - 1), 1);
 
     auto addr = BlockAddress{ 100000, 0 };
 
-    for (auto i = 0; i < 1024 * 1024; ++i) {
+    for (auto i = 0; i < number_of_index_entries; ++i) {
         index.append(i * 1024, addr);
         addr.add(1024);
     }
 
-    ASSERT_EQ(helper.number_of_blocks(BlockType::Index, 0, 10000), 3329);
+    ASSERT_EQ(helper.number_of_blocks(BlockType::Index, 0, blocks_required - 1), 3329);
 }
